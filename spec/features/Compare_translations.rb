6ff21087f9ddce8c56f6751697db860b24083c6c
if26 <if26@mac08-m102.utt.fr>
2015-06-05 13:00:43 +0200

    FIX: Add test pending

--- spec/features/Compare_translations.rb
+++ spec/features/Compare_translations.rb
@@ -1,8 +1,17 @@
 ﻿require 'spec_helper'
 
 feature 'Compare translations' do
+	background do
+        	visit '/works'
+        	click_on '+'
+        	fill_in 'title', :with => 'The lamp (Fungi from Yuggoth, 6)'
+        	fill_in 'author', :with => 'Howard Phillips Lovecraft'
+        	fill_in block(1,1), :with => 'We found the lamp inside those hollow cliffs'
+        	click_on 'Save'
+    	end
 
 	scenario 'Compare translations' do
+		pending 'Need to create traduction'
 		visit '/works'
 		click_on 'The lamp (Fungi from Yuggoth, 6)'
 		page.should have_content 'We found the lamp inside those hollow cliffs'
